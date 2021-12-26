6c0b9858d4a01081bd9015bbee0bc4aae5050bf4
h.valerio.filho <htecmac@gmail.com>
2021-12-26 12:48:27 -0300

    Google Services json

--- app/src/main/java/com/avanade/avamovies/MainActivity.kt
+++ app/src/main/java/com/avanade/avamovies/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.avanade.avamovies
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.avanade.avamovies.databinding.ActivityMainBinding
+import com.avanade.avamovies.view.CommentsActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+
+
+        binding.btnComments.setOnClickListener {
+            val intent = Intent(this,CommentsActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
