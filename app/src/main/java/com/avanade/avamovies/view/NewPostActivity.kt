5ae85e71ce79b8b75a97a71403e939a0769de1c6
Dannyel <dannyel_kayke@hotmail.com>
2021-12-27 13:24:56 -0300

    Criação da tela de nova postagem

---
+++ app/src/main/java/com/avanade/avamovies/view/NewPostActivity.kt
@@ -0,0 +1,12 @@
+package com.avanade.avamovies.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.avanade.avamovies.R
+
+class NewPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_post)
+    }
+}
